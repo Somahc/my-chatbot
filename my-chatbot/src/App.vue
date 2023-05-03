289032c21d72b93809fa70ca493c9f3e358f674d
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-05-03 21:04:07 +0900

    サイドバーをv-forを使って表示するよう変更

--- my-chatbot/src/App.vue
+++ my-chatbot/src/App.vue
@@ -1,30 +1,48 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+const items = [
+  {
+    title: 'ホーム',
+    path: './src/Home.vue',
+    icon: 'mdi-home'
+  },
+  {
+    title: 'アカウント',
+    path: '/Account',
+    icon: 'mdi-account-box'
+  },
+  {
+    title: 'チャット',
+    path: '/Chat',
+    icon: 'mdi-message'
+  },
+  {
+    title: '設定',
+    path: '/Setting',
+    icon: 'mdi-cog'
+  },
+]
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        class="bg-deep-purple"
+        theme="dark"
+        permanent
+      >
+        <v-list color="transparent">
+          <v-list-item v-for="item in items" active-color="primary" :to="item.path" class="py-3">
+            <v-icon :icon="item.icon" class="mr-6" />
+            <v-list-item-title class="font-weight-bold" v-text="item.title" />
+          </v-list-item>
+        </v-list>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+      </v-navigation-drawer>
+      <v-main style="height: 400px">
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
