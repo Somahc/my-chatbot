2f3e09279a1a779c95d184927f5c30661a5024a4
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-05-05 16:59:54 +0900

    メッセージボックスだけとりあえず実装

---
+++ my-chatbot/src/App copy.vue
@@ -0,0 +1,84 @@
+<script setup>
+
+
+const items = [
+  {
+    title: 'ホーム',
+    path: './src/Home.vue',
+    icon: 'mdi-home'
+  },
+  {
+    title: 'アカウント',
+    path: '/Account',
+    icon: 'mdi-account-box'
+  },
+  {
+    title: 'チャット',
+    path: '/Chat',
+    icon: 'mdi-message'
+  },
+  {
+    title: '設定',
+    path: '/Setting',
+    icon: 'mdi-cog'
+  },
+]
+</script>
+
+<template>
+  <v-app>
+    <v-card>
+      <v-layout>
+        <v-navigation-drawer
+          class="bg-deep-purple"
+          theme="dark"
+          permanent
+        >
+          <v-list color="transparent">
+            <v-list-item v-for="item in items" active-color="primary" :to="item.path" class="py-3">
+              <v-icon :icon="item.icon" class="mr-6" />
+              <v-list-item-title class="font-weight-bold" v-text="item.title" />
+            </v-list-item>
+          </v-list>
+
+        </v-navigation-drawer>
+        <v-main style="height: 800px">
+          
+          <v-form>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="message"
+                    :append-icon="mdi-send"
+                    :prepend-icon="icon"
+                    variant="filled"
+                    clear-icon="mdi-close-circle"
+                    clearable
+                    label="メッセージ"
+                    type="text"
+                    @click:append-inner="toggleMarker"
+                    @click:append="sendMessage"
+                    @click:prepend="changeIcon"
+                    @click:clear="clearMessage"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+
+
+
+
+
+        </v-main>
+      </v-layout>
+    </v-card>
+  </v-app>
+</template>
+
+<style>
+.textInputt {
+  outline: blue;
+}
+</style>
--- my-chatbot/src/App.vue
+++ my-chatbot/src/App.vue
@@ -1,48 +1,39 @@
 <script setup>
+import { ref } from 'vue'
+
+const message = ref("")
+
+const send_onClick = () => {
+  console.log(message.value);
+}
 
 
-const items = [
-  {
-    title: 'ホーム',
-    path: './src/Home.vue',
-    icon: 'mdi-home'
-  },
-  {
-    title: 'アカウント',
-    path: '/Account',
-    icon: 'mdi-account-box'
-  },
-  {
-    title: 'チャット',
-    path: '/Chat',
-    icon: 'mdi-message'
-  },
-  {
-    title: '設定',
-    path: '/Setting',
-    icon: 'mdi-cog'
-  },
-]
 </script>
 
 <template>
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer
-        class="bg-deep-purple"
-        theme="dark"
-        permanent
-      >
-        <v-list color="transparent">
-          <v-list-item v-for="item in items" active-color="primary" :to="item.path" class="py-3">
-            <v-icon :icon="item.icon" class="mr-6" />
-            <v-list-item-title class="font-weight-bold" v-text="item.title" />
-          </v-list-item>
-        </v-list>
-
-      </v-navigation-drawer>
-      <v-main style="height: 400px">
-      </v-main>
-    </v-layout>
-  </v-card>
+  <v-app>
+    <v-form>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="message"
+            label="Message"
+            type="text"
+            @keyup.enter="send_onClick"
+          >
+            <template v-slot:append>
+              <v-btn color="primary" @click="send_onClick">送信</v-btn>
+            </template>
+          </v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+  {{ message }}
+  </v-app>
 </template>
+
+<style>
+
+</style>
