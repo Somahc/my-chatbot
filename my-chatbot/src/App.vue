cca3d041d918c83060748e1f24c4a4473a80ff2e
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-05-10 00:25:53 +0900

    APIが使えるようになった

--- my-chatbot/src/App.vue
+++ my-chatbot/src/App.vue
@@ -2,122 +2,23 @@
 import { processExpression } from '@vue/compiler-core';
 import { ref } from 'vue'
 import { Configuration, OpenAIApi } from "openai";
-
-const API_KEY = "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";
-
-
+import { getResponse } from './components/getResponse'
 
 const message = ref("")
 
-const send_onClick = () => {
-  console.log(message.value);
-  //makePrompt(message.value);
-  completion();
-  message.value = "";
-
-}
-
-// APIキーを設定したオブジェクトを生成
-const openai = new OpenAIApi(new Configuration({ apiKey: API_KEY }))
-
-// ChatGPTに対する指示を以下に記述する --- (*2)
-const promptTest = 'こんにちは'
-
-// ChatGPTを呼び出す --- (*3)
-async function callAPI() {
-    const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: promptTest,
-    });
-    const answer = response.data.choices[0].text
-    console.log(answer)
-    console.log(response.data)
-}
-callAPI()
+const result = ref("結果")
 
 
-
-
-/*
-const configuration = new Configuration({
-  apiKey: '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',
-});
-const openai = new OpenAIApi(configuration);
-
-(async () => {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "chatGPTってなに？" }],
-  });
-  console.log(response.data.choices[0].text);
-  console.log("a");
-})();
-
-/*
-async function sendPrompt(prompt = '') {
-  const API_KEY = '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'
-
-  if (!prompt) return
-
-  const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${API_KEY}`,
-    },
-    body: JSON.stringify({
-      'model': 'gpt-3.5-turbo',
-      'prompt': prompt,
-      "max_tokens": 150,
-      "temperature": 1,
-    }),
-  })
-
-  const data = await response.json()
-  console.log(data.choices[0].text.trim())
-}
-
-let prompt = 'Hello'
-sendPrompt(prompt)
-
-
-
-
-
-/*
-//const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: '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',
-});
-const openai = new OpenAIApi(configuration);
-
-(async () => {
-  const response = await openai.createCompletion({
-    model: "gpt-3.5-turbo",
-    prompt: "Hello!",
-    temperature: 0,
-    max_tokens: 7,
-  });
-  console.log(response.data.choices[0].text);
-  console.log("a");
-})();
-*/
-
-/*
-const makePrompt = (userMessage) => {
-  const response = fetch(URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer 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'
-    },
-    body: JSON.stringify({
-      'model': 'gpt-3.5-turbo',
-      'stream'
+const send_onClick = async () => {
+  const response = await getResponse(message.value)
+    .then(response => response.json())
+    .then(data => {
+        const responseMessage = data.choices[0].message.content;
+        console.log(responseMessage)
+        result.value = responseMessage;
     })
-  })
 }
-*/
+
 </script>
 
 <template>
@@ -139,6 +40,7 @@ const makePrompt = (userMessage) => {
       </v-row>
     </v-container>
   </v-form>
+  {{ result }}
   </v-app>
 </template>
 
---
+++ my-chatbot/src/components/getResponse.js
@@ -0,0 +1,36 @@
+export async function getResponse(prompt = '') {
+    const API_KEY = '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'
+
+    console.log(prompt)
+    if (prompt == '') return
+
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'user', content: prompt }],
+            max_tokens: 150, // 出力される文章量の最大値（トークン数） max:4096
+            temperature: 1, // 単語のランダム性 min:0.1 max:2.0
+            top_p: 1, // 単語のランダム性 min:-2.0 max:2.0
+            frequency_penalty: 0.0, // 単語の再利用 min:-2.0 max:2.0
+            presence_penalty: 0.6 // 単語の再利用 min:-2.0 max:2.0
+            //stop: [' Human:', ' AI:'] // 途中で生成を停止する単語
+        })
+    })
+
+    return response;
+    /*
+    .then(response => response.json())
+    .then(data => {
+        const responseMessage = data.choices[0].message.content;
+        console.log(responseMessage)
+    })
+    .catch(() => {
+        console.log('エラー発生！')
+    })
+    */
+}
