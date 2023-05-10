bc6336778fedb497bbbc23455575bf511cd9284a
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-05-11 01:06:39 +0900

    吹き出しデザイン（仮）

--- my-chatbot/src/App.vue
+++ my-chatbot/src/App.vue
@@ -9,8 +9,10 @@ const message = ref("");
 const result = ref("結果");
 const messageHistory = ref([]);
 
+//初期メッセージ
 messageHistory.value.push(makePrompt('assistant', 'こんにちは！なにかお手伝いできることはありますか？'));
 
+//送信ボタンクリック時
 const send_onClick = async () => {
   const prompt = makePrompt('user', message.value);
   messageHistory.value.push(prompt);
@@ -24,46 +26,61 @@ const send_onClick = async () => {
 <template>
   <v-app>
     <div class="chatContainer">
-    <div v-for="message in messageHistory">
-      <div v-if="message.role == 'assistant'" class="aiMessage">
-        {{ message.content }}
+      <div v-for="message in messageHistory" class="messageBox">
+        <div v-if="message.role == 'assistant'" class="aiMessage">
+          {{ message.content }}
+        </div>
+        <div v-if="message.role == 'user'" class="userMessage">
+          {{ message.content }}
+        </div>
       </div>
-      <div v-if="message.role == 'user'" class="userMessage">
-        {{ message.content }}
-      </div>
+      <v-form>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="message"
+              type="text"
+              placeholder="メッセージを送信"
+            >
+              <template v-slot:append>
+                <v-btn color="primary" @click="send_onClick">送信</v-btn>
+              </template>
+            </v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
     </div>
-    <v-form>
-    <v-container>
-      <v-row>
-        <v-col>
-          <v-text-field
-            v-model="message"
-            type="text"
-            placeholder="メッセージを送信"
-          >
-            <template v-slot:append>
-              <v-btn color="primary" @click="send_onClick">送信</v-btn>
-            </template>
-          </v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
-</div>
   </v-app>
 </template>
 
 <style>
 .aiMessage{
+  border-radius: 0px 10px 10px 10px;
   background-color: antiquewhite;
+  margin-right: 200px;
+  padding: 15px;
+  font-size: large;
+
 }
 
 .userMessage{
+  border-radius: 10px 0px 10px 10px;
   background-color: aquamarine;
+  text-align: right;
+  margin-left: 200px;
+  padding: 15px 15px 15px 15px;
+  font-size: large;
 }
 
 .chatContainer{
-  width: 80%;
+  width: 75%;
   margin: 0 auto;
+}
+
+.messageBox{
+  padding-top: 4px;
+  padding-bottom: 4px;
 }
 </style>
