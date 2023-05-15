48eae62f79a7f645fe4ed365e753ca4ab60d6230
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-05-15 19:06:05 +0900

    App.vueの不要なコードを削除

--- my-chatbot/src/App.vue
+++ my-chatbot/src/App.vue
@@ -1,23 +1,4 @@
 <script setup>
-import { processExpression } from '@vue/compiler-core';
-import { ref } from 'vue'
-import { getResponse } from './components/getResponse';
-import makePrompt from './components/makePrompt'
-
-const message = ref("");
-const messageHistory = ref([]);
-
-//初期メッセージ
-messageHistory.value.push(makePrompt('assistant', 'こんにちは！なにかお手伝いできることはありますか？'));
-
-//送信ボタンクリック時
-const send_onClick = async () => {
-  const prompt = makePrompt('user', message.value);
-  messageHistory.value.push(prompt);
-  const response = await getResponse(prompt);
-  message.value = '';
-  messageHistory.value.push(response);
-}
 
 </script>
 
@@ -29,33 +10,5 @@ const send_onClick = async () => {
 </template>
 
 <style>
-.aiMessage{
-  border-radius: 0px 10px 10px 10px;
-  background-color: antiquewhite;
-  margin-right: 200px;
-  padding: 15px;
-  font-size: large;
-  font-weight: 700;
 
-}
-
-.userMessage{
-  border-radius: 10px 0px 10px 10px;
-  background-color: aquamarine;
-  text-align: right;
-  margin-left: 200px;
-  padding: 15px 15px 15px 15px;
-  font-size: large;
-  font-weight: 700;
-}
-
-.chatContainer{
-  width: 75%;
-  margin: 0 auto;
-}
-
-.messageBox{
-  padding-top: 4px;
-  padding-bottom: 4px;
-}
 </style>
