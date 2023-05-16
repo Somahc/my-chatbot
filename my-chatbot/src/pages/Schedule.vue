f7b33447d95c6b0586ef4a3961e2aded9134900a
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-05-16 15:03:11 +0900

    とりあえずScheduleページにテキストボックスをコピペ

--- my-chatbot/src/pages/Schedule.vue
+++ my-chatbot/src/pages/Schedule.vue
@@ -3,5 +3,22 @@
 </script>
 
 <template>
-    <h2>スケジュールのテスト</h2>
+    <v-form>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <div v-if="isLoading" class="loading">ChatGPTがタイプ中...</div>
+                    <v-text-field
+                    v-model="message"
+                    type="text"
+                    placeholder="メッセージを送信"
+                    >
+                        <template v-slot:append>
+                            <v-btn color="primary" @click="send_onClick">送信</v-btn>
+                        </template>
+                    </v-text-field>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
 </template>
