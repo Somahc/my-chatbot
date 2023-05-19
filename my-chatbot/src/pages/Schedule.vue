cbb345f989c456d83209d812836ca340e93c7d87
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-05-19 23:47:40 +0900

    月初めの曜日番号を取得

--- my-chatbot/src/pages/Schedule.vue
+++ my-chatbot/src/pages/Schedule.vue
@@ -1,10 +1,16 @@
 <script setup>
 import Calendar from '../components/Calendar.vue';
+import moment from "moment";
+import { onMounted } from 'vue';
+
+onMounted(() => {
+   let date = moment().startOf("month");
+   const youbiNum = date.day();
+   console.log(youbiNum) 
+});
 
 </script>
 
 <template>
      <v-calendar expanded></v-calendar>
 </template>
-  
- 
