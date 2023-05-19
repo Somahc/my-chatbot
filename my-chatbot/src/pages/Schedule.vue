a07dd579d886022d5c92f6b1aaec3b428407f0c9
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-05-19 23:57:15 +0900

    カレンダー終わりの日を取得

--- my-chatbot/src/pages/Schedule.vue
+++ my-chatbot/src/pages/Schedule.vue
@@ -12,8 +12,16 @@ const getStartDate = () => {
     return date.subtract(youbiNum, "days");
 }
 
+const getEndDate = () => {
+    let date = moment(currentDate);
+    date.endOf("month");
+    const youbiNum = date.day();
+    return date.add(6 - youbiNum, "days");
+}
+
 onMounted(() => {
     console.log(getStartDate())
+    console.log(getEndDate())
 })
 
 </script>
