29c1a755bf70ce3381aafadd00167616aaf43d88
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-06-01 01:31:54 +0900

    サイドバーを追加

--- my-chatbot/src/App.vue
+++ my-chatbot/src/App.vue
@@ -1,13 +1,52 @@
 <script setup>
+const sidebarContent = [
+  {name: 'Top', href: '/', icon: UserIcon }
+]
 
+import {
+    Bars3BottomLeftIcon,
+    BellIcon,
+    CalendarIcon,
+    ChartBarIcon,
+    FolderIcon,
+    HomeIcon,
+    InboxIcon,
+    UsersIcon,
+    XMarkIcon,
+    ChatBubbleLeftEllipsisIcon,
+    CommandLineIcon,
+    DocumentTextIcon,
+    AcademicCapIcon,
+    CloudIcon,
+    LockClosedIcon,
+    CurrencyYenIcon,
+    Cog8ToothIcon,
+    ChatBubbleLeftRightIcon,
+    UserIcon
+} from '@heroicons/vue/24/outline'
 </script>
 
 <template>
   <v-app>
-    <nav>
-      <router-link to="/">Top</router-link>  | <router-link to="/chat">Chat</router-link>  | <router-link to="/schedule">Schedule</router-link>
-    </nav>
-    <router-view />
+    <v-navigation-drawer
+      app
+      dark
+      clipped
+      :width="200"
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item prepend-icon="mdi-home" title="Top" href="/"></v-list-item>
+        <v-list-item prepend-icon="mdi-chat" title="Chat" href="/chat"></v-list-item>
+        <v-list-item prepend-icon="mdi-calendar-month" title="Schedule" href="/schedule"></v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
